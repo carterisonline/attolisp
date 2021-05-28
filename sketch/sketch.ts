79d81db8c05d0d21353ebf754f015010b467be37
Carter Reeb <carter2k4@pm.me>
2021-05-27 20:04:34 -0500

    Fixed issues with custom values & nested functions

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -2,76 +2,6 @@
 //! import * as p5Global from 'p5/global'
 //! import * as p5 from 'p5'
 
-// Tokenize the program on the simplest level; the first step.
-function tokenize(chars: string): string[] {
-  return chars
-    .replace(/\(/g, " ( ") //   Add whitespace to parenthesis so that they can be
-    .replace(/\)/g, " ) ") //   easily parsed by the `read_from_tokens()` stage.
-    .split(/\s+/g) // Treat any length of whitespace as a separator.
-    .filter((c) => c !== ""); // Filter out empty instructions
-}
-
-// Function for dealing with basic primitives
-function atom(token: string): Atom<any> {
-  // Attempt to parse this as an integer
-  const int = parseInt(token, 10);
-
-  //                Fix for old EMCAStript parseInt() handling
-  //                 |                         |
-  //                 v                         v
-  if (!isNaN(int) && (int !== 0 && token === "0") === false) {
-    return new AtNum(int);
-  }
-
-  // Not an int? Let's try a float.
-  const float = parseFloat(token);
-  if (!isNaN(float)) {
-    return new AtNum(float);
-  }
-
-  // Handle booleans
-  if (token === "false") {
-    return new AtBool(false);
-  }
-
-  if (token === "true") {
-    return new AtBool(true);
-  }
-
-  // None of the above? Just return a string.
-  return new AtSymb(token);
-}
-
-// Function responsible for parsing the tokens into actual data
-function read_from_tokens(tokens: string[]): any {
-  //   Since `read_from_tokens()` is recursive, each pair of parenthesis are treated as their
-  //   own "program," and while doing such, any unexpected EOF (end-of-file) will be treated as
-  //   a parser error. This can also happen when the function is passed an empty list of instructions.
-  if (tokens.length === 0) {
-    throw new Error("You missed a parenthesis, or your program is empty!");
-  }
-  const token = tokens.shift(); // Pop off item from the front of `tokens`
-  if (token === "(") {
-    // Parse a pair of parenthesis
-    let subpair = [];
-    while (tokens[0] !== ")") {
-      // Munch through all of the instructions inside 🤤
-      subpair.push(read_from_tokens(tokens));
-    }
-    tokens.pop(); // Pop off the last closing parenthesis
-    return subpair;
-  } else if (token === ")") {
-    // GRRR! HOW MANY TIMES TO I HAVE TO TELL YOU THIS ⁉️
-    throw new Error("Unexpected ')'!");
-  } else {
-    return atom(token); // Just return a literal is there's no subpair.
-  }
-}
-
-function parse(program: string): any {
-  return read_from_tokens(tokenize(program));
-}
-
 // Default environment (the Env class is located at Env.ts)
 let global_env: Env;
 
@@ -79,12 +9,10 @@ function eval_a(
   x: Atom<any> | any[],
   env: Env = global_env
 ): Atom<any> | Function {
-  console.log("x is:");
-  console.log(x);
   if (x instanceof AtSymb) {
     // Return function if argument passed is a string (not a literal)
     return env.data[x.val];
-  } else if (x instanceof AtNum) {
+  } else if (x instanceof AtNum || x instanceof AtBool) {
     // You don't have to do anything with numbers
     return x;
   } else if ((x as AtSymb[])[0].val === "if") {
@@ -95,32 +23,33 @@ function eval_a(
   } else if ((x as AtSymb[])[0].val === "define") {
     // Add a function to the environment, persisting only for the scope it's in.
     const [_, symbol, exp] = x as any[];
-    env.data[symbol] = eval_a(exp, env);
+    env.data[(symbol as AtSymb).val] = eval_a(exp, env);
+    console.log(env);
   } else {
     // If `x` is an array/object, then we have to run a subpair as a function!
-    const proc: Function = eval_a((x as any[])[0], env) as Function; // What function are we using?
-    (x as any[]).shift(); // Pop the instruction from the subpair
-    return proc(x); // Run the function with the arguments!
+    const proc = eval_a((x as any[])[0], env); // What function are we using?
+
+    if (typeof proc === "function") {
+      let args = []; // Run the function with the arguments!
+      for (let i = 1; i < (x as any[]).length; i++) {
+        args.push(eval_a((x as any[])[i], env));
+      }
+      return proc(args);
+    }
   }
 }
 
 function setup() {
-  let program = "(* 2 (+ 2 2))";
+  let program = "(/ 81 3 3)";
 
   console.log(`Your input: ${program}`);
+  console.log("Is tokenized into: ")
 
   global_env = Env.default();
 
   const parsed = parse(program);
 
-  console.log("Tokenized input:");
-  console.log(tokenize(program));
-
-  console.log("Parsed input:");
   console.log(parsed);
-
-  console.log("Global environment / core library:");
-  console.log(global_env);
 
   let out = eval_a(parsed);
 
